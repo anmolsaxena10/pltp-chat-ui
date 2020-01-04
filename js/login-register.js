01ce59468b3849a94ee7f51b75f1c9e3f7a0def1
Anmol <anmol.saxena@jovianinc.com>
2020-01-04 21:00:55 +0530

    login register functionality completed

--- js/login-register.js
+++ js/login-register.js
@@ -1,9 +1,61 @@
-var login = function(){
+var api_url = "http://localhost:8899/api/";
+
+var login = function () {
     $('#login_btn').attr("disabled", true);
-    
+    var username = $("#lusername").val();
+    var password = $("#lpassword").val();
+    $.ajax({
+        url: api_url + "login",
+        type: "POST",
+        data: {
+            username: username,
+            password: password
+        },
+        success: function (result) {
+            console.log(result);
+            if (!result.success) {
+                $('#login_btn').attr("disabled", false);
+                M.toast({html: result.message});
+            }
+            else {
+                M.toast({html: 'Logged In'});
+                localStorage.setItem("token", result.token);
+                window.location.href = "./users.html";
+            }
+        }
+    });
+    console.log(username, password);
 }
 
-var register = function(){
+var register = function () {
     console.log("re");
     $('#register_btn').attr("disabled", true);
+    var username = $("#rusername").val();
+    var password = $("#rpassword").val();
+    var cpassword = $("#rcpassword").val();
+    if (password != cpassword) {
+        $('#register_btn').attr("disabled", false);
+        M.toast({html: 'Password does not match'});
+    }
+    else {
+        $.ajax({
+            url: api_url + "addUser",
+            type: "POST",
+            data: {
+                username: username,
+                password: password
+            },
+            success: function (result) {
+                console.log(result);
+                if (!result.success) {
+                    $('#register_btn').attr("disabled", false);
+                    M.toast({html: result.message});
+                }
+                else {
+                    M.toast({html: 'Registered Succesfully, Please Login'});
+                }
+            }
+        });
+    }
+    console.log(username, password);
 }
